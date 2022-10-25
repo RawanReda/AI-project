e5dda3bd0c3bc4df90ffa096d9ccae441c93aa15
rawan <rawan.reda.academia@gmail.com>
2022-10-25 20:00:13 +0200

    project setup

---
+++ src/code/CoastGaurd.java
@@ -0,0 +1,12 @@
+package code;
+
+public class CoastGaurd extends SearchProblem{
+
+    public static int[][] genGrid(){
+        return null;
+    }
+
+    public static String solve(String grid, String strategy, Boolean visualise){
+        return "";
+    }
+}
